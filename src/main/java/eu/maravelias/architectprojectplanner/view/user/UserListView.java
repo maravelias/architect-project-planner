2be1278e067a9ae5308566ec7dce74af6e3d17fc
Giorgos Maravelias <maravelias@netmechanics.gr>
2026-01-23 10:44:47 +0200

    Initial commit

---
+++ src/main/java/eu/maravelias/architectprojectplanner/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package eu.maravelias.architectprojectplanner.view.user;
+
+import eu.maravelias.architectprojectplanner.entity.User;
+import eu.maravelias.architectprojectplanner.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "app_User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
